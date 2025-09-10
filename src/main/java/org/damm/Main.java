edb2e0be5bf704988d148325f5fabde33fc98e0f
diegoxze35 <diegoalv1217@gmail.com>
2025-09-10 00:39:23 -0600

    stored procedures

--- src/main/java/org/damm/Main.java
+++ src/main/java/org/damm/Main.java
@@ -1,22 +1,31 @@
 package org.damm;
 
+import java.sql.SQLException;
 import org.damm.domain.dao.CategoryDao;
 import org.damm.domain.dao.Dao;
 import org.damm.domain.pojo.Category;
 
-import java.sql.SQLException;
-
 public class Main {
 	public static void main(String[] args) throws SQLException {
-		Dao<Category> categoryDao = new CategoryDao("mysql", "localhost", 3306, "categories_db", "diego", "1234");
+		Dao<Category> categoryDao = new CategoryDao(
+				"mysql",
+				"localhost",
+				3306,
+				"categories_db",
+				"diego",
+				"123"
+		);
 		Category c = new Category();
 		c.setName("Category 1");
 		c.setDescription("Description 1");
-		categoryDao.save(c);
+		System.out.println("INSERT " + categoryDao.save(c));
 		c.setDescription("Updated Description 1");
-		categoryDao.save(c);
+		System.out.println("UPDATE " + categoryDao.save(c));
 		for (Category cc : categoryDao.findAll()) {
 			System.out.println(cc);
 		}
+
+		System.out.println("SELECT BY ID " + categoryDao.findById(1));
+		categoryDao.delete(c);
 	}
 }
--- src/main/java/org/damm/domain/dao/CategoryDao.java
+++ src/main/java/org/damm/domain/dao/CategoryDao.java
@@ -1,25 +1,23 @@
 package org.damm.domain.dao;
 
-import org.damm.domain.pojo.Category;
-
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
+import org.damm.domain.pojo.Category;
 
 public class CategoryDao implements Dao<Category> {
-	
-	private static final String SQL_INSERT = "call create_category(?, ?)";
-	private static final String SQL_SELECT = "SELECT * FROM category WHERE id_category = ?";
+
+	private static final String SQL_INSERT = "call create_category(?, ?, ?)";
+	private static final String SQL_SELECT = "call select_one(?)";
 	private static final String SQL_DELETE = "call delete_category(?)";
 	private static final String SQL_UPDATE = "call update_category(?, ?, ?)";
-	private static final String SQL_SELECT_ALL = "SELECT * FROM category";
-	
+	private static final String SQL_SELECT_ALL = "call select_all()";
+
 	private static Category mapResultSetToCategory(ResultSet resultSet) throws SQLException {
 		return new Category(
 				resultSet.getInt("id_category"),
@@ -27,18 +25,53 @@ public class CategoryDao implements Dao<Category> {
 				resultSet.getString("description")
 		);
 	}
-	
-	private final Connection connection;
-	
+
+	private Connection connection;
+	private final String driver;
+	private final String host;
+	private final int port;
+	private final String databaseName;
+	private final String user;
+	private final String password;
+
 	public CategoryDao(String driver, String host, int port, String databaseName, String user, String password) throws SQLException {
+		this.driver = driver;
+		this.host = host;
+		this.port = port;
+		this.databaseName = databaseName;
+		this.user = user;
+		this.password = password;
+		connectToDatabase();
+	}
+
+	private void connectToDatabase() throws SQLException {
 		String url = "jdbc:" + driver + "://" + host + ":" + port + "/" + databaseName;
 		connection = DriverManager.getConnection(url, user, password);
 	}
-	
+
+	private void connect() {
+		try {
+			connectToDatabase();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void closeConnection() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			connection = null;
+		}
+	}
+
 	@Override
 	public Category findById(Integer id) {
+		connect();
 		Category category = null;
-		try (PreparedStatement statement = connection.prepareStatement(SQL_SELECT)) {
+		try (CallableStatement statement = connection.prepareCall(SQL_SELECT)) {
 			statement.setInt(1, id);
 			ResultSet resultSet = statement.executeQuery();
 			if (resultSet.next()) category = mapResultSetToCategory(resultSet);
@@ -49,13 +82,14 @@ public class CategoryDao implements Dao<Category> {
 		}
 		return category;
 	}
-	
+
 	@Override
 	public List<Category> findAll() {
+		connect();
 		List<Category> categories = new ArrayList<>();
 		try (
-				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery(SQL_SELECT_ALL)
+				CallableStatement statement = connection.prepareCall(SQL_SELECT_ALL);
+				ResultSet resultSet = statement.executeQuery()
 		) {
 			while (resultSet.next()) {
 				categories.add(mapResultSetToCategory(resultSet));
@@ -67,14 +101,16 @@ public class CategoryDao implements Dao<Category> {
 		}
 		return categories;
 	}
-	
+
 	@Override
 	public Category save(Category entity) {
+		connect();
 		return entity.getIdCategory() == null ? insertCategory(entity) : updateCategory(entity);
 	}
-	
+
 	@Override
 	public void delete(Category entity) {
+		connect();
 		try (CallableStatement statement = connection.prepareCall(SQL_DELETE)) {
 			statement.setInt(1, entity.getIdCategory());
 			statement.executeUpdate();
@@ -84,7 +120,7 @@ public class CategoryDao implements Dao<Category> {
 			closeConnection();
 		}
 	}
-	
+
 	private Category updateCategory(Category entity) {
 		try (CallableStatement statement = connection.prepareCall(SQL_UPDATE)) {
 			statement.setInt(1, entity.getIdCategory());
@@ -98,17 +134,14 @@ public class CategoryDao implements Dao<Category> {
 		}
 		return entity;
 	}
-	
+
 	private Category insertCategory(Category entity) {
 		try (CallableStatement statement = connection.prepareCall(SQL_INSERT)) {
 			statement.setString(1, entity.getName());
 			statement.setString(2, entity.getDescription());
-			statement.executeUpdate();
-			// Retrieve the generated ID
-			ResultSet generatedKeys = statement.getGeneratedKeys();
-			if (generatedKeys.next()) {
-				entity.setIdCategory(generatedKeys.getInt(1));
-			}
+			statement.registerOutParameter(3, Types.INTEGER);
+			statement.execute();
+			entity.setIdCategory(statement.getInt(3));
 		} catch (SQLException e) {
 			throw new RuntimeException("Error saving category", e);
 		} finally {
@@ -116,17 +149,5 @@ public class CategoryDao implements Dao<Category> {
 		}
 		return entity;
 	}
-	
-	private void connect() {
-	
-	}
-	
-	private void closeConnection() {
-		try {
-			connection.close();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
+
 }
